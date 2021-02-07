9c28677373a538d40acfe4614f1f65f67168b73f
MarilenaRoque <marilenas@id.uff.br>
2021-02-07 14:58:46 -0300

    Providing Store to App Component

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import { getMovies } from './api/requests'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 const movies = getMovies()
 movies.then(data => console.log(data.results))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
       <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-
