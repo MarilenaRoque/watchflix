74f37ec127a6cc42ae89820c71dd46f89cec8c4f
MarilenaRoque <marilenas@id.uff.br>
2021-02-07 15:21:51 -0300

    Set mapStateToProps
    Passing props to MovieList component

--- src/containers/MovieList.js
+++ src/containers/MovieList.js
@@ -1,23 +1,27 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
-import store from '../redux/store'
-import {useEffect} from 'react';
-import moviesRequest from '../redux/thunkFunction'
+import store from '../redux/store';
+import { useEffect } from 'react';
+import moviesRequest from '../redux/thunkFunction';
+import { connect } from 'react-redux';
 
-const MovieList = props => {
+
+const MovieList = ({ moviesList }) => {
     console.log(store.getState())
     useEffect(() => {
         moviesRequest();
     });
     return (
         <div>
-            <h1>{store.requesting}</h1>
+            <h1>{moviesList}</h1>
         </div>
     )
 }
 
+const mapStateToProps = state => ({ moviesList: state.movies.movies.join() });
 // MovieList.propTypes = {
 
 // }
+export default connect( mapStateToProps )( MovieList );
 
-export default MovieList
+
