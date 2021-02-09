ca6676f607d8d5e27fdae81c1b9b0546eaf23da2
MarilenaRoque <marilenas@id.uff.br>
2021-02-08 21:22:07 -0300

    [FIX]Add react-router-dom dependency

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -6,9 +6,11 @@ const MoviePage = (props) => {
 
     const { id } = useParams();
 
+    
+
     return (
         <div>
-            <h1>{'dddd'}</h1>
+            <h1>{id}</h1>
         </div>
     )
 }
