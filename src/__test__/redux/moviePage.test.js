b3b3b6464976929a147dc6f4455b415de3f2fc3d
MarilenaRoque <marilenas@id.uff.br>
2021-02-10 20:38:46 -0300

    Testing MoviePage Reducer

---
+++ src/__test__/redux/moviePage.test.js
@@ -0,0 +1,48 @@
+import moviePage from '../../redux/reducers/moviePage';
+
+const exampleMovies = { title: 'movie1' };
+
+const exampleActionSuccess = {
+  type: 'SET_MOVIE_SUCCESS',
+  payload: exampleMovies,
+};
+
+const exampleActionFailure = {
+  type: 'SET_MOVIE_FAILURE',
+  payload: 404,
+};
+
+const initialState = {
+  movie: null,
+  loading: false,
+  error: null,
+};
+
+describe('state of movie is changed when a SET_MOVIE_SUCCESS is dispatched', () => {
+  it('it should change the state using the payload object', () => {
+    const result = moviePage(initialState, exampleActionSuccess);
+    expect(result.movie).toEqual(exampleMovies);
+  });
+
+  it('it should reset the loading state', () => {
+    const result = moviePage(initialState, exampleActionSuccess);
+    expect(result.loading).toEqual(false);
+  });
+});
+
+describe('state of movie is changed when a SET_MOVIE_FAILURE is dispatched', () => {
+  it('it should change the state using the payload object', () => {
+    const result = moviePage(initialState, exampleActionFailure);
+    expect(result.movie).toEqual(null);
+  });
+
+  it('it should keep the error', () => {
+    const result = moviePage(initialState, exampleActionFailure);
+    expect(result.error).toEqual(404);
+  });
+
+  it('it should reset the loading state', () => {
+    const result = moviePage(initialState, exampleActionFailure);
+    expect(result.loading).toEqual(false);
+  });
+});
